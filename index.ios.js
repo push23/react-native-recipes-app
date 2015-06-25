59addb6627db5245de6d6ebe16a90d10d2cfda67
Nick Gladkiy <nickg@wix.com>
2015-06-25 12:11:53 +0300

    Created app folder and move initial view there

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,8 @@
  */
 'use strict';
 
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+var AppRegistry = require('react-native').AppRegistry;
 
-var AwesomeRecipes = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
+var Application = require('./app/app.js');
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('AwesomeRecipes', () => AwesomeRecipes);
+AppRegistry.registerComponent('AwesomeRecipes', () => Application);
